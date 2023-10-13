231daace531a78506450e41124048f1ac4ae3e7f
ahmetkage <9021567@student.zadkine.nl>
2023-10-13 08:45:56 +0200

    6.4

---
+++ opdracht6.4/opdracht6.4.js
@@ -0,0 +1,15 @@
+function calculate(num1, num2) {
+    const result = num1 + num2;
+    alert(`Oefening 1: ${num1} + ${num2} = ${result}`);
+  }
+  
+  function multiplyByFive(num) {
+    const result = num * 5;
+    alert(`Oefening 2: ${num} * 5 = ${result}`);
+  }
+  
+  function calculateMinutes(seconds) {
+    const minutes = seconds / 60;
+    alert(`Oefening 3: ${seconds} seconden is gelijk aan ${minutes} minuten`);
+  }
+  
